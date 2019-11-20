9c70fd967c825cff5a2a45ba083f3aab140ffa5d
iva-nova-vita <ivanovavita89@gmail.com>
2019-11-20 02:06:17 +0200

    change sliders style

--- JS/slider.js
+++ JS/slider.js
@@ -1,4 +1,37 @@
 let slider = document.getElementsByClassName("slider");
 $(() => {
-  $(slider).slick();
+  $(slider).slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      
+    ]
+  });
 });
